b0ed2d8a0715585bb8c266baade28e4a00246e3f
Paweł Bucior <pawel@bucior.eu>
2020-02-28 23:00:44 +0100

    DatabaseHandler deleteContact

--- app/src/main/java/eu/bucior/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/eu/bucior/contactmanager/data/DatabaseHandler.java
@@ -98,4 +98,13 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         return db.update(Util.TABLE_NAME, values, Util.KEY_ID + "=?",
                 new String[]{String.valueOf(contact.getId())});
     }
+
+    public void deleteContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(Util.TABLE_NAME, Util.KEY_ID + "=?",
+                new String[]{String.valueOf(contact.getId())});
+
+        db.close();
+    }
 }
