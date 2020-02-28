d3b7022eb38a66898be7189a400227a1ddd8b6b9
Paweł Bucior <pawel@bucior.eu>
2020-02-28 22:55:45 +0100

    DatabaseHandler updateContact

--- app/src/main/java/eu/bucior/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/eu/bucior/contactmanager/data/DatabaseHandler.java
@@ -87,4 +87,15 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         return contactList;
     }
+
+    public int updateContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME, contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        return db.update(Util.TABLE_NAME, values, Util.KEY_ID + "=?",
+                new String[]{String.valueOf(contact.getId())});
+    }
 }
