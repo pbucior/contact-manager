0a8d96829bef8447c78762dc064bf0c70875db51
Paweł Bucior <pawel@bucior.eu>
2020-02-28 21:40:18 +0100

    DatabaseHandler configuration

--- app/src/main/java/eu/bucior/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/eu/bucior/contactmanager/data/DatabaseHandler.java
@@ -4,21 +4,30 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
+import eu.bucior.contactmanager.R;
+import eu.bucior.contactmanager.util.Util;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY,"
+                + Util.KEY_NAME + " TEXT,"
+                + Util.KEY_PHONE_NUMBER + " TEXT)";
 
+        db.execSQL(CREATE_CONTACT_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String DROP_TABLE = String.valueOf(R.string.db_drop);
+        db.execSQL(DROP_TABLE, new String[]{Util.TABLE_NAME});
 
+        onCreate(db);
     }
 }
